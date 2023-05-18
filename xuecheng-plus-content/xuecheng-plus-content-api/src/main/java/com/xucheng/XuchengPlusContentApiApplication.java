f38fc6ce77bf93ce7e8eca50ace2e7fbf29c181e
zhangshuaibao922 <zhangshuaibao922@163.com>
2023-05-18 15:09:58 +0800

    提交项目模块基础

---
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xucheng/XuchengPlusContentApiApplication.java
@@ -0,0 +1,13 @@
+package com.xucheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuchengPlusContentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuchengPlusContentApiApplication.class, args);
+    }
+
+}
