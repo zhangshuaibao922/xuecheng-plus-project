5bee99339ce811e892abf438d47cf7f4c4b262c7
zhangshuaibao922 <zhangshuaibao922@163.com>
2023-05-20 11:18:05 +0800

    提交第二天完整代码

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/execption/ValidationGroups.java
@@ -0,0 +1,15 @@
+package com.xuecheng.base.execption;
+
+/**
+ * @author cardo
+ * @Version 1.0
+ * @Description TODO
+ * @date 2023/5/20 11:01
+ */
+public class ValidationGroups {
+
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+
+}
